d34d9fa2e4b0bc9d4b5a6bbe02da9fdbdc6ea445
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2021-12-16 12:47:43 +0100

    Added simple function and start deafult bloginfo wp function to learn about it.

---
+++ wp-content/themes/it-university/index.php
@@ -0,0 +1,12 @@
+<?php
+
+  function greet($name, $color){
+    echo "<p>Hi my name is $name, my favourite color is: $color</p>";
+  }
+
+  greet('Sebastian', 'green');
+  greet('Klaudia','blue');
+
+?>
+
+<h1><?php bloginfo('name'); ?></h1>
