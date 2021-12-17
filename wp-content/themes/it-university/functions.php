9859dac3297fd2457034a82e2dd2adb2650a578d
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2021-12-17 17:31:16 +0100

    Added custo google fonts

--- wp-content/themes/it-university/functions.php
+++ wp-content/themes/it-university/functions.php
@@ -2,6 +2,7 @@
 
 function it_university_files(){
   //Added function which loaded main style.css file
+  wp_enqueue_style('custom-google-fonts','//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
   wp_enqueue_style('font_awesome','//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
   wp_enqueue_style('it_university_main_styles',get_theme_file_uri('./build/style-index.css'));
   wp_enqueue_style('it_university_extra_styles',get_theme_file_uri('./build/index.css'));
