460fcdf999f4c5c3ea4e162892225aad58454135
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2021-12-17 17:21:56 +0100

    Added font awesome link in header by function.php

--- wp-content/themes/it-university/functions.php
+++ wp-content/themes/it-university/functions.php
@@ -2,6 +2,7 @@
 
 function it_university_files(){
   //Added function which loaded main style.css file
+  wp_enqueue_style('font_awesome','//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
   wp_enqueue_style('it_university_main_styles',get_theme_file_uri('./build/style-index.css'));
   wp_enqueue_style('it_university_extra_styles',get_theme_file_uri('./build/index.css'));
 }
