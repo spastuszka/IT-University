35b52d830f657f30a402889fde65838d77ea3954
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2021-12-17 10:45:34 +0100

    create footer.php and reference in index.php

--- wp-content/themes/it-university/index.php
+++ wp-content/themes/it-university/index.php
@@ -1,12 +1,16 @@
 <?php
 
-//Wczytanie blog postow i ich opublikowanie na stronie
+//nagłówek domyślny
+  get_header(); 
+
+  //content danej strony
 
   while(have_posts()){
     the_post(); ?>
       <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
       <p><?php the_content(); ?></p>
     <?php
-  }
+  } 
 
+  get_footer();
 ?>
