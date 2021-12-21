44ec73e650ff0753164a41de3041cae1a7e7d613
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2021-12-21 17:59:19 +0100

    Changed hardcoded linke and title in widget menu to dinamicly

--- wp-content/themes/it-university/page.php
+++ wp-content/themes/it-university/page.php
@@ -33,7 +33,7 @@
     ?>
 
       <div class="page-links">
-        <h2 class="page-links__title"><a href="#">About Us</a></h2>
+        <h2 class="page-links__title"><a href="<?php echo get_permalink($theParent); ?>"><?php echo get_the_title($theParent); ?></a></h2>
         <ul class="min-list">
           <?php
 
