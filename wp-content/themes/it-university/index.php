ea06367ecb9ee7c10c6b7a5e6cdd18cc995179ab
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2021-12-22 22:53:30 +0100

    Added custom class and change post title style

--- wp-content/themes/it-university/index.php
+++ wp-content/themes/it-university/index.php
@@ -16,7 +16,7 @@
         the_post();?>
 
         <div class="post-item">
-          <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+          <h2 class="headline headline--medium headline--post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
           <div class="metabox">
             <p>Posted by: <?php  ?> on <?php ?> in <?php ?></p>
           </div>
