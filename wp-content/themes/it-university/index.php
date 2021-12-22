4d45af8a569d82024bdfdba8c0c596b1051c5e8c
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2021-12-22 22:30:43 +0100

    Create blog page with custom post list

--- wp-content/themes/it-university/index.php
+++ wp-content/themes/it-university/index.php
@@ -7,5 +7,24 @@
           <p>TODO: Don't Forget to replace this text by custom post type</p>
         </div>
       </div>
-    </div>
+  </div>
+
+  <div class="container container--narrow page-section">
+    <?php
+
+    while(have_posts()){
+        the_post();?>
+
+        <div class="post-item">
+          <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+          <div class="metabox">
+            <p>Posted by: <?php  ?> on <?php ?> in <?php ?></p>
+          </div>
+          <div class="generic-content">
+            <p><?php the_excerpt(); ?></p>
+          </div>
+        </div>
+
+    <?php } ?>
+  </div>
 <?php get_footer(); ?>
