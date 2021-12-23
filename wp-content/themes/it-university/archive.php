79e986a41076c1b71e016fa6f46bc4f4bcfd756f
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2021-12-23 15:32:13 +0100

    Added logic to add archive header

--- wp-content/themes/it-university/archive.php
+++ wp-content/themes/it-university/archive.php
@@ -2,7 +2,13 @@
   <div class="page-banner">
       <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg'); ?>)"></div>
       <div class="page-banner__content container container--narrow">
-        <h1 class="page-banner__title">Welcome to our blog!</h1>
+        <h1 class="page-banner__title"><?php if(is_category()){
+          single_cat_title();
+        }elseif(is_author()){
+          echo 'Posts by '; the_author();
+        }else{
+          echo "date will go here";
+        }  ?></h1>
         <div class="page-banner__intro">
           <p>TODO: Don't Forget to replace this text by custom post type</p>
         </div>
