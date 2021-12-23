967d981492de557343becdd95271633ebdc31cb5
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2021-12-23 15:17:09 +0100

    Change info about post in metabox

--- wp-content/themes/it-university/single.php
+++ wp-content/themes/it-university/single.php
@@ -20,7 +20,7 @@ get_header();
     <div class="container container--narrow page-section">
       <div class="metabox metabox--position-up metabox--with-home-link">
         <p>
-          <a class="metabox__blog-home-link" href="<?php echo site_url('/blog'); ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to Blog</a><span class="metabox__main"><?php the_title(); ?></span>
+          <a class="metabox__blog-home-link" href="<?php echo site_url('/blog'); ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to Blog</a><span class="metabox__main">Posted by: <?php the_author_posts_link(); ?> on <?php the_time('j M Y'); ?> in <?php echo get_the_category_list(', ') ?></span>
         </p>
       </div>
       <div class="generic-content">
