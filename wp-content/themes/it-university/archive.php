4fa06912bc0bdfef4d8596d9aa40ae61e76e5c3c
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2021-12-23 15:36:00 +0100

    Commented logic specific archive title to wp function which create title in archive pages

--- wp-content/themes/it-university/archive.php
+++ wp-content/themes/it-university/archive.php
@@ -2,13 +2,14 @@
   <div class="page-banner">
       <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg'); ?>)"></div>
       <div class="page-banner__content container container--narrow">
-        <h1 class="page-banner__title"><?php if(is_category()){
+        <h1 class="page-banner__title"><?php the_archive_title();
+        /*if(is_category()){
           single_cat_title();
         }elseif(is_author()){
           echo 'Posts by '; the_author();
         }else{
           echo "date will go here";
-        }  ?></h1>
+        }  */?></h1>
         <div class="page-banner__intro">
           <p>TODO: Don't Forget to replace this text by custom post type</p>
         </div>
