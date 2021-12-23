aa75e320c8d322ffb292fbc043174e90d3845b8c
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2021-12-23 16:37:33 +0100

    Create dynamic display time from recenet posts

--- wp-content/themes/it-university/front-page.php
+++ wp-content/themes/it-university/front-page.php
@@ -51,8 +51,8 @@
               $homepagePosts->the_post();?>
                 <div class="event-summary">
                   <a class="event-summary__date event-summary__date--beige t-center" href="#">
-                    <span class="event-summary__month">Jan</span>
-                    <span class="event-summary__day">20</span>
+                    <span class="event-summary__month"><?php the_time('M'); ?></span>
+                    <span class="event-summary__day"><?php the_time('d'); ?></span>
                   </a>
                   <div class="event-summary__content">
                     <h5 class="event-summary__title headline headline--tiny"><a href="#"><?php the_title(); ?></a></h5>
