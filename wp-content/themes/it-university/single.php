ff9c1a9f821be29dad511e328d4f39484c4d8dbb
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2021-12-23 14:55:41 +0100

    Added page banner from page.php

--- wp-content/themes/it-university/single.php
+++ wp-content/themes/it-university/single.php
@@ -7,11 +7,27 @@ get_header();
 
   while(have_posts()){
     the_post(); ?>
-      <h2><?php the_title(); ?></h2>
-      <p><?php the_content(); ?></p>
-    <?php
-  }
+      <div class="page-banner">
+      <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg'); ?>)"></div>
+      <div class="page-banner__content container container--narrow">
+        <h1 class="page-banner__title"><?php the_title(); ?></h1>
+        <div class="page-banner__intro">
+          <p>TODO: Don't Forget to replace this text by custom post type</p>
+        </div>
+      </div>
+    </div>
 
-  //default footer
+    <div class="container container--narrow page-section">
+      <div class="metabox metabox--position-up metabox--with-home-link">
+        <p>
+          <a class="metabox__blog-home-link" href="<?php echo get_permalink($theParent); ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to Blog</a><span class="metabox__main"><?php the_title(); ?></span>
+        </p>
+      </div>
+      <div class="generic-content">
+        <?php the_content(); ?>
+      </div>
+    </div>
+    <?php
+  }//default footer
   get_footer();
 ?>
