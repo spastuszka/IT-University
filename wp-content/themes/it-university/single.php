543b85390aec0bc0244833d29f8075feea695492
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2021-12-23 14:59:20 +0100

    Changed metabox link to blog page

--- wp-content/themes/it-university/single.php
+++ wp-content/themes/it-university/single.php
@@ -20,7 +20,7 @@ get_header();
     <div class="container container--narrow page-section">
       <div class="metabox metabox--position-up metabox--with-home-link">
         <p>
-          <a class="metabox__blog-home-link" href="<?php echo get_permalink($theParent); ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to Blog</a><span class="metabox__main"><?php the_title(); ?></span>
+          <a class="metabox__blog-home-link" href="<?php echo site_url('/blog'); ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to Blog</a><span class="metabox__main"><?php the_title(); ?></span>
         </p>
       </div>
       <div class="generic-content">
