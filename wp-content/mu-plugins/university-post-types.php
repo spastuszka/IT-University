813c7949b8ed724c798951f27709b70e988e5b2e
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2021-12-24 22:56:32 +0100

    Create custom php file and paste funtion which create custom post type

---
+++ wp-content/mu-plugins/university-post-types.php
@@ -0,0 +1,15 @@
+<?php
+
+function university_post_types(){
+  register_post_type('event',
+    array(
+      'public' => true,
+      'labels' => array(
+        'name' => __('Events'),
+        'singular_name' => __('Event')
+      ),
+      'menu_icon' => 'dashicons-calendar',
+    ));
+}
+
+add_action('init', 'university_post_types');
