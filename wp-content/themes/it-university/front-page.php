19369d74c24d3a7db0171a6f97cba91b28603a6c
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2021-12-25 12:03:12 +0100

    Added dynamicly link and date from custom post type - event

--- wp-content/themes/it-university/front-page.php
+++ wp-content/themes/it-university/front-page.php
@@ -24,9 +24,9 @@
             while($homepageEvents -> have_posts()){
               $homepageEvents -> the_post();?>
               <div class="event-summary">
-                <a class="event-summary__date t-center" href="#">
-                  <span class="event-summary__month">Mar</span>
-                  <span class="event-summary__day">25</span>
+                <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
+                  <span class="event-summary__month"><?php the_time('M');?></span>
+                  <span class="event-summary__day"><?php the_time('d');?></span>
                 </a>
                 <div class="event-summary__content">
                   <h5 class="event-summary__title headline headline--tiny"><a href="#">Poetry in the 100</a></h5>
