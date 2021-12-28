564ef062e1404868e5b6be9c55380937d084c741
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2021-12-28 15:34:18 +0100

    Added sorted event by custom meta value about custom field

--- wp-content/themes/it-university/front-page.php
+++ wp-content/themes/it-university/front-page.php
@@ -19,7 +19,8 @@
             $homepageEvents = new WP_Query(array(
               'posts_per_page' => -1,
               'post_type' => 'event',
-              'orderby' => 'title',
+              'meta_key' => 'event_date',
+              'orderby' => 'meta_value_num',
               'order' => 'ASC',
             ));
 
