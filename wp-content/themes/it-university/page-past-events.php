43e39ad886fd98889555b699be50444270580d73
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2021-12-28 17:04:04 +0100

    Change description about new page past events

--- wp-content/themes/it-university/page-past-events.php
+++ wp-content/themes/it-university/page-past-events.php
@@ -4,7 +4,7 @@
       <div class="page-banner__content container container--narrow">
         <h1 class="page-banner__title">Past Events</h1>
         <div class="page-banner__intro">
-          <p>See what is going on in our world.</p>
+          <p>A recap of our past events.</p>
         </div>
       </div>
   </div>
