89b4ceabacd4866afdada8d146ad4680c735741d
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2021-12-28 15:48:03 +0100

    Created variable which tell the current date

--- wp-content/themes/it-university/front-page.php
+++ wp-content/themes/it-university/front-page.php
@@ -15,7 +15,7 @@
         <div class="full-width-split__inner">
           <h2 class="headline headline--small-plus t-center">Upcoming Events</h2>
           <?php
-          
+            $today = date('Ymd');
             $homepageEvents = new WP_Query(array(
               'posts_per_page' => -1,
               'post_type' => 'event',
