cfd8a0444fd8a41d8fa730e528ba78bc1e2fee6b
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2021-12-29 20:28:57 +0100

    Added thumbnail in CPT Professor

--- wp-content/mu-plugins/university-post-types.php
+++ wp-content/mu-plugins/university-post-types.php
@@ -44,7 +44,7 @@ function university_post_types(){
     //Professor Post Type
   register_post_type('professor',
   array(
-    'supports' => array('title', 'editor'),
+    'supports' => array('title', 'editor','thumbnail'),
     'public' => true,
     'show_in_rest' => true,
     'labels' => array(
