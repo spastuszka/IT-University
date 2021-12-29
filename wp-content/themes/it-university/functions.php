6c9b55d751ed1235407516d725a46e86f66b6a5f
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2021-12-29 20:21:26 +0100

    Changed function name and add theme support about feature image

--- wp-content/themes/it-university/functions.php
+++ wp-content/themes/it-university/functions.php
@@ -13,14 +13,16 @@ function it_university_files(){
 add_action( 'wp_enqueue_scripts', 'it_university_files' );
 
 
-function it_university_title(){
+function it_university_features(){
   register_nav_menu('headerMenuLocation','Header Menu Location');
   register_nav_menu('footerLocationOne','Footer Location One');
   register_nav_menu('footerLocationTwo','Footer Location Two');
   add_theme_support('title-tag');
+  add_theme_support('post-thumbnails');
+
 }
 
-add_action('after_setup_theme','it_university_title');
+add_action('after_setup_theme','it_university_feature');
 
 
 function it_university_adjust_queries($query){
