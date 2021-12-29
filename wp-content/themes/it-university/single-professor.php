fe7153b8dd1e098e279988c8e85ca17aa725a7d7
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2021-12-29 19:44:57 +0100

    Removed metabox in CF Professor

--- wp-content/themes/it-university/single-professor.php
+++ wp-content/themes/it-university/single-professor.php
@@ -18,11 +18,7 @@ get_header();
     </div>
 
     <div class="container container--narrow page-section">
-      <div class="metabox metabox--position-up metabox--with-home-link">
-        <p>
-          <a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('event'); ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to Events</a><span class="metabox__main"><?php the_title(); ?></span>
-        </p>
-      </div>
+     
       <div class="generic-content">
         <?php the_content(); ?>
       </div>
