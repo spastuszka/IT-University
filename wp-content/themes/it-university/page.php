c7820dec1798bcd13ccd7f86ad2c1b8d7e9aa039
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2021-12-30 19:49:46 +0100

    Added function in single profesro template and page template

--- wp-content/themes/it-university/page.php
+++ wp-content/themes/it-university/page.php
@@ -2,18 +2,11 @@
 //Wczytanie blog postow i ich opublikowanie na stronie
 
   while(have_posts()){
-    the_post(); ?>
+    the_post();
     
-    <div class="page-banner">
-      <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg'); ?>)"></div>
-      <div class="page-banner__content container container--narrow">
-        <h1 class="page-banner__title"><?php the_title(); ?></h1>
-        <div class="page-banner__intro">
-          <p>TODO: Don't Forget to replace this text by custom post type</p>
-        </div>
-      </div>
-    </div>
+    pageBanner();
 
+    ?>
     
 
     <div class="container container--narrow page-section">
