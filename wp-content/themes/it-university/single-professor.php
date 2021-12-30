06105ed717f27ed1b4c627a9edce7c183009bbb2
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2021-12-30 17:01:51 +0100

    Changed layout for thubnail and content

--- wp-content/themes/it-university/single-professor.php
+++ wp-content/themes/it-university/single-professor.php
@@ -20,7 +20,10 @@ get_header();
     <div class="container container--narrow page-section">
 
       <div class="generic-content">
-        <?php the_post_thumbnail(); the_content(); ?>
+        <div class="row group">
+          <div class="one-third"><?php the_post_thumbnail(); ?></div>
+          <div class="two-thirds"><?php the_content(); ?></div>
+        </div>
       </div>
 
       <?php
