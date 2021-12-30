f643ab0c24297489fe743cb403c0a2dee229bdd2
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2021-12-30 18:54:11 +0100

    Added new image size

--- wp-content/themes/it-university/functions.php
+++ wp-content/themes/it-university/functions.php
@@ -21,6 +21,7 @@ function it_university_title(){
   add_theme_support('post-thumbnails');
   add_image_size( 'professorLandscape', 400, 260, true );
   add_image_size( 'professorPortrait', 480, 650, true );
+  add_image_size( 'pageBanner', 1500, 350, true );
 
 }
 
