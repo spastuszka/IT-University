c53af08b372bdb2aae209a90ad660ebcfb8c27ab
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2022-01-14 19:55:17 +0100

    Deleted some duplicated code

--- wp-content/themes/it-university/single-program.php
+++ wp-content/themes/it-university/single-program.php
@@ -89,12 +89,15 @@ get_header();
           $relatedCampuses = get_field('related_campus');
 
           if($relatedCampuses){
+
+            echo '<hr class="section-break">';
+
             echo '<h2 class="headline headline--medium">'. get_the_title().' is available at these campuses.</h2>';
 
             echo '<ul class="min-list link-list">';
 
             foreach($relatedCampuses as $campus){
-              ?> <li><a href="<?php get_the_permalink($campus); ?>"><?php get_the_title($campus); ?></a></li><?php
+              ?> <li><a href="<?php echo get_the_permalink($campus); ?>"><?php echo get_the_title($campus); ?></a></li><?php
             }
 
             echo '</ul>';
