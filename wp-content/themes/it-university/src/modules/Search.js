0c46ecd874bbd85fe65f04d05eeeb5ed198f6995
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2022-01-15 18:40:43 +0100

    Addded search js file

---
+++ wp-content/themes/it-university/src/modules/Search.js
@@ -0,0 +1,30 @@
+import $ from 'jquery';
+
+class Search {
+  /* 1. Describe and create/initiate our object */
+  constructor(){
+    this.openButton = $('.js-search-trigger');
+    this.closeButton = $('.search-overlay__close');
+    this.searchOverlay = $('.search-overlay');
+    this.events();
+  }
+
+  /* 2. Events */
+  events(){
+    this.openButton.on("click", this.openOverlay.bind(this));
+    this.closeButton.on("click",this.closeOverlay.bind(this));
+  }
+
+  /* 3. Methods (function, action...) */
+  openOverlay(){
+    this.searchOverlay.addClass('search-overlay--active');
+    $("body").addClass("body-no-scroll");
+  }
+
+  closeOverlay(){
+    this.searchOverlay.removeClass('search-overlay--active');
+    $("body").removeClass("body-no-scroll");
+  }
+}
+
+export default Search
