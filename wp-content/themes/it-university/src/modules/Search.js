6cb414e03265a02b4276e283da7df08cc0696b2e
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2022-01-16 18:35:08 +0100

    Added test search typing logic

--- wp-content/themes/it-university/src/modules/Search.js
+++ wp-content/themes/it-university/src/modules/Search.js
@@ -6,6 +6,7 @@ class Search {
     this.openButton = $('.js-search-trigger');
     this.closeButton = $('.search-overlay__close');
     this.searchOverlay = $('.search-overlay');
+    this.searchField = $('#search-term');
     this.events();
     this.isOverlayOpen = false;
   }
@@ -15,9 +16,14 @@ class Search {
     this.openButton.on("click", this.openOverlay.bind(this));
     this.closeButton.on("click",this.closeOverlay.bind(this));
     $(document).on("keydown", this.keyPressDispatcher.bind(this));
+    this.searchField.on("keydown",this.typingLogic.bind(this));
   }
 
   /* 3. Methods (function, action...) */
+
+  typingLogic(){
+    alert("typing logic");
+  }
 
   keyPressDispatcher(e){
     if(e.keyCode == 83 && !this.isOverlayOpen){
