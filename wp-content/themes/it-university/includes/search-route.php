4049305de9412342f5983774b279114d03305bf3
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2022-01-24 20:12:58 +0100

    Added custom Raw JSON data

--- wp-content/themes/it-university/includes/search-route.php
+++ wp-content/themes/it-university/includes/search-route.php
@@ -8,7 +8,21 @@ function universityRegisterSearch(){
 }
 
 function universitySearchResults(){
-  return 'Testowy route, działa!';
+  $professors = new WP_Query(array(
+    'post_type' => 'professor',
+  ));
+
+  $professorResults = array();
+
+  while($professors->have_posts()){
+    $professors->the_post();
+    array_push($professorResults, array(
+      'title' => get_the_title(),
+      'permalink' => get_the_permalink(),
+    ));
+  }
+
+  return $professorResults;
 }
 
 add_action('rest_api_init','universityRegisterSearch');
