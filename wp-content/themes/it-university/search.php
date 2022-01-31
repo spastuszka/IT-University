2eedb7953ebcb41ddccea270dab2e111252af392
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2022-01-31 21:36:23 +0100

    added custom searchform file and some fix modification

--- wp-content/themes/it-university/search.php
+++ wp-content/themes/it-university/search.php
@@ -7,9 +7,7 @@ pageBanner(array(
 
 <div class="container container--narrow page-section">
   <?php
-
   if (have_posts()) {
-
     while (have_posts()) {
       the_post();
       get_template_part('template-parts/content', get_post_type());
@@ -19,6 +17,12 @@ pageBanner(array(
     echo '<h2 class="headline headline--small-plus">No results match that search.</h2>';
   }
 
+  get_search_form();
+
   ?>
+
 </div>
-<?php get_footer(); ?>
+
+<?php get_footer();
+
+?>
