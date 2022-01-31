d40c4cb3c2e9e1a5de280cf60420367044911765
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2022-01-31 21:00:44 +0100

    Changed header in search php file

--- wp-content/themes/it-university/search.php
+++ wp-content/themes/it-university/search.php
@@ -1,7 +1,7 @@
 <?php get_header();
   pageBanner(array(
-    'title' => 'Welcome to our blog!',
-    'subtitle' => 'Keep up with our latest news.',
+    'title' => 'Search Results',
+    'subtitle' => 'You searched for &ldquo;'. get_search_query() .'&rdquo;',
   ));
 ?>
 
