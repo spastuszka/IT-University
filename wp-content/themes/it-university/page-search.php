7fabff30094da2071f03e08ea58f7ddb5eb104d3
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2022-01-31 20:50:41 +0100

    Added some styling form in search page

--- wp-content/themes/it-university/page-search.php
+++ wp-content/themes/it-university/page-search.php
@@ -56,9 +56,12 @@
       <?php } ?>
 
       <div class="generic-content">
-        <form method="get" action="<?php echo esc_url(site_url('/')); ?>">
-          <input type="search" name="s">
-          <input type="submit" value="Search">
+        <form class="search-form" method="get" action="<?php echo esc_url(site_url('/')); ?>">
+        <label class="headline headline--medium" for="s">Perform a New Search:</label>
+          <div class="search-form-row">
+            <input class="s" id="s" type="search" name="s">
+            <input class="search-submit" type="submit" value="Search">
+          </div>
         </form>
       </div>
     </div>
