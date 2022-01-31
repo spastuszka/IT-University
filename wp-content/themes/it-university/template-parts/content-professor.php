0b353ea463b654f64228f82617b2aec46ed2f315
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2022-01-31 21:24:03 +0100

    Modify view element in search results page

---
+++ wp-content/themes/it-university/template-parts/content-professor.php
@@ -0,0 +1,8 @@
+<div class="post-item">
+  <li class="professor-card__list-item">
+    <a class="professor-card" href="<?php echo get_the_permalink($program); ?>">
+      <img class="professor-card__image" src="<?php the_post_thumbnail_url('professorLandscape'); ?>">
+      <span class="professor-card__name"><?php the_title(); ?></span>
+    </a>
+  </li>
+</div>
