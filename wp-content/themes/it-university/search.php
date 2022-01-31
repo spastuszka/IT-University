07058a777fc74bf1c6ec4a351cff56974b7c6c6d
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2022-01-31 21:30:55 +0100

    Seacure when no results form

--- wp-content/themes/it-university/search.php
+++ wp-content/themes/it-university/search.php
@@ -1,22 +1,24 @@
 <?php get_header();
 pageBanner(array(
   'title' => 'Search Results',
-  'subtitle' => 'You searched for &ldquo;' . esc_url(get_search_query()) . '&rdquo;',
+  'subtitle' => 'You searched for &ldquo;' . esc_url(get_search_query(false)) . '&rdquo;',
 ));
 ?>
 
 <div class="container container--narrow page-section">
   <?php
 
-  while (have_posts()) {
-    the_post();
-    get_template_part('template-parts/content', get_post_type());
-  ?>
+  if (have_posts()) {
 
+    while (have_posts()) {
+      the_post();
+      get_template_part('template-parts/content', get_post_type());
+    }
+    echo paginate_links();
+  } else {
+    echo '<h2 class="headline headline--small-plus">No results match that search.</h2>';
+  }
 
-
-  <?php }
-  echo paginate_links();
   ?>
 </div>
 <?php get_footer(); ?>
