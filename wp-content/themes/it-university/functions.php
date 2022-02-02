d402ca8be15fff95bd7525e78f1817b46005c515
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2022-02-02 18:59:50 +0100

    Hide admin bar for subscriber login account and fix some function about it

--- wp-content/themes/it-university/functions.php
+++ wp-content/themes/it-university/functions.php
@@ -119,9 +119,20 @@ function redirectSubsToFrontend()
 {
   $ourCurrentUser = wp_get_current_user();
 
-  if (count($ourCurrentUser->roles) == 1 and $ourCurrentUser->role[0] == 'subscriber') {
+  if (count($ourCurrentUser->roles) == 1 and $ourCurrentUser->roles[0] == 'subscriber') {
     wp_redirect(site_url('/'));
     exit;
   }
 }
 add_action('admin_init', 'redirectSubsToFrontend');
+
+//Hide admin bar when subcriber is login
+function noSubsAdminBar()
+{
+  $ourCurrentUser = wp_get_current_user();
+
+  if (count($ourCurrentUser->roles) == 1 and $ourCurrentUser->roles[0] == 'subscriber') {
+    show_admin_bar(false);
+  }
+}
+add_action('wp_loaded', 'noSubsAdminBar');
