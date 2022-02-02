9a1428259d003ba162100a083404f3b5de0bbef4
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2022-02-02 19:17:15 +0100

    Changed login default logo - turn on loaded css

--- wp-content/themes/it-university/functions.php
+++ wp-content/themes/it-university/functions.php
@@ -145,3 +145,16 @@ function outHeaderUrl()
 }
 
 add_filter('login_headerurl', 'outHeaderUrl');
+
+
+//Changed login default logo - turn on loaded css
+
+function ourLoginCSS()
+{
+  wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+  wp_enqueue_style('font_awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+  wp_enqueue_style('it_university_main_styles', get_theme_file_uri('./build/style-index.css'));
+  wp_enqueue_style('it_university_extra_styles', get_theme_file_uri('./build/index.css'));
+}
+
+add_action('login_enqueue_scripts', 'ourLoginCSS');
