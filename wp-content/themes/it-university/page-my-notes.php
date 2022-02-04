1aede16c699026fd1e41f0b399f336ef13a237d5
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2022-02-04 19:31:08 +0100

    Created data attribute and push id js

--- wp-content/themes/it-university/page-my-notes.php
+++ wp-content/themes/it-university/page-my-notes.php
@@ -25,8 +25,8 @@ while (have_posts()) {
 
       while ($userNotes->have_posts()) {
         $userNotes->the_post(); ?>
-        <li>
-          <input class="note-title-field" value="<?php echo esc_attr(get_the_title()); ?>">
+      <li data-id="<?php the_ID(); ?>">
+          <input class=" note-title-field" value="<?php echo esc_attr(get_the_title()); ?>">
           <span class="edit-note"><i class="fa fa-pencil" aria-hidden="true"></i> Edit</span>
           <span class="delete-note"><i class="fa fa-trash-o" aria-hidden="true"></i> Delete</span>
           <textarea class="note-body-field"><?php echo esc_attr(wp_strip_all_tags(get_the_content())); ?></textarea>
