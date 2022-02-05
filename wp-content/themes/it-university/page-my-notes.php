9cfc14e4bc4d698f5d9d34a9e97ee9f48b6568e2
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2022-02-05 19:29:03 +0100

    Created html form to input title and body new notes

--- wp-content/themes/it-university/page-my-notes.php
+++ wp-content/themes/it-university/page-my-notes.php
@@ -15,6 +15,14 @@ while (have_posts()) {
 
 
   <div class="container container--narrow page-section">
+
+    <div class="create-note">
+      <h2 class="headline  headline--medium">Create New Note</h2>
+      <input class="new-note-title" placeholder="Title" type="text">
+      <textarea class="new-note-body" placeholder="Your note here..." name="" id="" cols="30" rows="10"></textarea>
+      <span class="submit-note">Create Note</span>
+    </div>
+
     <ul class="min-list link-list" id="my-notes">
       <?php
       $userNotes = new WP_Query(array(
