31811b36bff1dd22c9d5923fa88ea5fca611038e
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2022-02-08 06:43:18 +0100

    Limited create note

--- wp-content/themes/it-university/functions.php
+++ wp-content/themes/it-university/functions.php
@@ -173,6 +173,11 @@ add_filter('login_headertitle', 'ourLoginTitle');
 function makeNotePrivate($data)
 {
   if ($data['post_type'] == 'note') {
+
+    if (count_user_posts(get_current_user_id(), 'note') > 5) {
+      die('Youe have reached your note limit.');
+    }
+
     $data['post_title'] = sanitize_text_field($data['post_title']);
     $data['post_content'] = sanitize_textarea_field($data['post_content']);
   }
