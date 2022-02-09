5231e74b7dedac7aa84149112a786cc5551e9e4d
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2022-02-09 20:01:37 +0100

    Added to professor single template html like counter

--- wp-content/themes/it-university/single-professor.php
+++ wp-content/themes/it-university/single-professor.php
@@ -5,37 +5,44 @@ get_header();
 
 //Upload blog posts and publish them on the site
 
-  while(have_posts()){
-    the_post(); 
-    pageBanner();
-    ?>
+while (have_posts()) {
+  the_post();
+  pageBanner();
+?>
 
-    <div class="container container--narrow page-section">
+  <div class="container container--narrow page-section">
 
-      <div class="generic-content">
-        <div class="row group">
-          <div class="one-third"><?php the_post_thumbnail('professorPortrait'); ?></div>
-          <div class="two-thirds"><?php the_content(); ?></div>
+    <div class="generic-content">
+      <div class="row group">
+        <div class="one-third"><?php the_post_thumbnail('professorPortrait'); ?></div>
+        <div class="two-thirds">
+          <span class="like-box">
+            <i class="fa fa-heart-o" aria-hidden="true"></i>
+            <i class="fa fa-heart" aria-hidden="true"></i>
+            <span class="like-count">3</span>
+          </span>
+          <?php the_content(); ?>
         </div>
       </div>
-
-      <?php
-        $relatedPrograms = get_field('related_programs');
-
-        if($relatedPrograms){
-          echo '<hr class="section-break">';
-          echo '<h3 class="headline headline--medium">Subject(s) Taught</h3>';
-          echo '<ul class="link-list min-list">';
-          foreach($relatedPrograms as $program){ ?>
-            <li><a href="<?php echo get_the_permalink($program); ?>"><?php echo get_the_title($program); ?></a></li>
-          <?php
-          }
-          echo '</ul>';
-        }
-
-      ?>
     </div>
+
     <?php
-  }//default footer
-  get_footer();
+    $relatedPrograms = get_field('related_programs');
+
+    if ($relatedPrograms) {
+      echo '<hr class="section-break">';
+      echo '<h3 class="headline headline--medium">Subject(s) Taught</h3>';
+      echo '<ul class="link-list min-list">';
+      foreach ($relatedPrograms as $program) { ?>
+        <li><a href="<?php echo get_the_permalink($program); ?>"><?php echo get_the_title($program); ?></a></li>
+    <?php
+      }
+      echo '</ul>';
+    }
+
+    ?>
+  </div>
+<?php
+} //default footer
+get_footer();
 ?>
